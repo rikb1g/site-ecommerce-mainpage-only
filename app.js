c62de110c7955e93a391530b1e4b35291505090a
Ricardo Sousa <r.sousa.888@gmail.com>
2024-01-25 15:30:04 +0000

    final

---
+++ app.js
@@ -0,0 +1,7 @@
+const navbar = document.querySelector(".navbar")
+const menuButton = document.querySelector(".menu-button")
+
+
+menuButton.addEventListener('click',() => {
+    navbar.classList.toggle("show-menu")
+});
